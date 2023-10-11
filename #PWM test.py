8ae37f7bfc35f6f3f242c47143fa0f20bfc98c11
Ekkodrom <you@example.com>
2023-10-11 13:13:35 +0200

    4 wheel drive

--- #PWM test.py
+++ #PWM test.py
@@ -12,8 +12,15 @@ SpeedPin = 33
 SpeedPin1 = 35
 # PWM pins
 
+
+SpeedPinA = 12
+SpeedPinA1 = 32
+
 DirectionPin = 16
 DirectionPin1 = 18
+
+DirectionPinA = 11
+DirectionPinA1 = 13
 
 GPIO.setwarnings(False)			#disable warnings
 GPIO.setmode(GPIO.BCM)	#set pin numbering system
@@ -23,18 +30,33 @@ GPIO.cleanup()
 GPIO.setup(SpeedPin,GPIO.OUT)
 GPIO.setup(SpeedPin1,GPIO.OUT)
 
+GPIO.setup(SpeedPinA,GPIO.OUT)
+GPIO.setup(SpeedPinA1,GPIO.OUT)
+
 GPIO.setup(DirectionPin,GPIO.OUT)
 GPIO.setup(DirectionPin1,GPIO.OUT)
+
+GPIO.setup(DirectionPinA,GPIO.OUT)
+GPIO.setup(DirectionPinA1,GPIO.OUT)
 
 
 pi_pwm = GPIO.PWM(SpeedPin,1000)		#create PWM instance with frequency
 pi_pwm.start(0)
 
 pi_pwm1 = GPIO.PWM(SpeedPin1,1000)		#create PWM instance with frequency
-pi_pwm1.start(0)			
+pi_pwm1.start(0)	
+
+pi_pwm1 = GPIO.PWM(SpeedPinA,1000)		#create PWM instance with frequency
+pi_pwm1.start(0)
+
+pi_pwm1 = GPIO.PWM(SpeedPinA1,1000)		#create PWM instance with frequency
+pi_pwm1.start(0)
 
 GPIO.output(DirectionPin, True)
 GPIO.output(DirectionPin1, True)
+
+GPIO.output(DirectionPinA, True)
+GPIO.output(DirectionPinA1, True)
 
 
 #start PWM of required Duty Cycle 
